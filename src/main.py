d1ef03be683768a5fdf4111c36c56baa8ee912ef
Irith Sharma <sharma.irith@gmail.com>
2025-10-17 11:51:27 -0400

    Run linter

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from src.config import load_env_vars
 
 env_vars = load_env_vars()
 
+
 def main():
     # number of pages of movies to get from the API. 1 page is 20 movies.
     pages = 1
@@ -10,15 +11,18 @@ def main():
     # api authorization
     headers = {
         "accept": "application/json",
-        "Authorization": "Bearer " + env_vars["TMDB_API_TOKEN"]
+        "Authorization": "Bearer " + env_vars["TMDB_API_TOKEN"],
     }
 
-    data = toprated_movies(pages, headers) # gets movie data from the TMDB API
-    data = extract_movie_data(data) # removes any animated movies from the list and any unnecessary data
-    data = actors(data, headers) # adds the top actors to the json
-    data = actor_images(data, headers) # adds the top actors images to the json
-    data = related_movies(data, headers) # find any related movies that could be alternative answers
-
+    data = toprated_movies(pages, headers)  # gets movie data from the TMDB API
+    data = extract_movie_data(
+        data
+    )  # removes any animated movies from the list and any unnecessary data
+    data = actors(data, headers)  # adds the top actors to the json
+    data = actor_images(data, headers)  # adds the top actors images to the json
+    data = related_movies(
+        data, headers
+    )  # find any related movies that could be alternative answers
 
 
 if __name__ == "__main__":
